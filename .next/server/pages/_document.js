08f30b3ba054ddae368e76afd74a8f7fb5b29b39
camnguyen37h <camnguyen37h@gmail.com>
2022-06-20 14:33:29 +0700

    fix/reload-server-styled-component

--- .next/server/pages/_document.js
+++ .next/server/pages/_document.js
@@ -10,13 +10,18 @@ exports.modules = {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */   "default": () => (/* binding */ MyDocument)
 /* harmony export */ });
 /* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(7518);
 /* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var next_document__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6859);
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(997);
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__);
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 
 
@@ -25,24 +30,38 @@ __webpack_require__.r(__webpack_exports__);
 class MyDocument extends next_document__WEBPACK_IMPORTED_MODULE_1__["default"] {
   static async getInitialProps(ctx) {
     const sheet = new styled_components__WEBPACK_IMPORTED_MODULE_0__.ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage; // Run the React rendering logic synchronously
+    const originalRenderPage = ctx.renderPage;
 
-    ctx.renderPage = () => originalRenderPage({
-      // Useful for wrapping the whole react tree
-      enhanceApp: App => App,
-      // Useful for wrapping in a per-page basis
-      enhanceComponent: Component => Component
-    }); // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const enhanceApp = App => props => sheet.collectStyles( /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(App, _objectSpread({}, props)));
 
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp
+      });
 
-    const initialProps = await next_document__WEBPACK_IMPORTED_MODULE_1__["default"].getInitialProps(ctx);
-    return initialProps;
+      const initialProps = await next_document__WEBPACK_IMPORTED_MODULE_1__["default"].getInitialProps(ctx);
+      return _objectSpread(_objectSpread({}, initialProps), {}, {
+        styles: (initialProps.styles, sheet.getStyleElement())
+      });
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
     return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(next_document__WEBPACK_IMPORTED_MODULE_1__.Html, {
-      children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(next_document__WEBPACK_IMPORTED_MODULE_1__.Head, {
-        children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx("link", {})
+      children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(next_document__WEBPACK_IMPORTED_MODULE_1__.Head, {
+        children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx("link", {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com"
+        }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx("link", {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossOrigin: "anonymous"
+        }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx("link", {
+          href: "https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700;800;900&display=swap",
+          rel: "stylesheet"
+        })]
       }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)("body", {
         children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(next_document__WEBPACK_IMPORTED_MODULE_1__.Main, {}), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx(next_document__WEBPACK_IMPORTED_MODULE_1__.NextScript, {})]
       })]
@@ -50,9 +69,6 @@ class MyDocument extends next_document__WEBPACK_IMPORTED_MODULE_1__["default"] {
   }
 
 }
-
-;
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyDocument);
 
 /***/ }),
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,45 +1,42 @@
-import { ServerStyleSheet } from "styled-components";
-import Document, {
-  Html,
-  Main,
-  Head,
-  NextScript,
-  DocumentInitialProps,
-} from "next/document";
+import { ServerStyleSheet } from 'styled-components';
+import Document, { Html, Main, Head, NextScript, DocumentInitialProps } from 'next/document';
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
     const sheet: ServerStyleSheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage
+    const originalRenderPage = ctx.renderPage;
+    const enhanceApp = (App: any) => (props: any) => sheet.collectStyles(<App {...props} />);
 
-    // Run the React rendering logic synchronously
-    ctx.renderPage = () =>
-      originalRenderPage({
-        // Useful for wrapping the whole react tree
-        enhanceApp: (App: any) => App,
-        // Useful for wrapping in a per-page basis
-        enhanceComponent: (Component: any) => Component,
-      })
+    try {
+      ctx.renderPage = () => originalRenderPage({ enhanceApp });
 
-    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
-    const initialProps = await Document.getInitialProps(ctx)
+      const initialProps: DocumentInitialProps = await Document.getInitialProps(ctx);
 
-    return initialProps
+      return {
+        ...initialProps,
+        styles: (initialProps.styles, sheet.getStyleElement()),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
     return (
       <Html>
         <Head>
-          <link />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
-};
-
-export default MyDocument;
+}
