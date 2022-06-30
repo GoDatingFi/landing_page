f8a689abc8bc9a8c137a2467f483a4099ccbbd88
camnguyen37h <camnguyen37h@gmail.com>
2022-06-30 18:00:45 +0700

    fix/change-teams-member

---
+++ src/utils/page/teams.const.ts
@@ -0,0 +1,146 @@
+import Leadership01 from 'assets/images/team/leadership/leadership-01.jpeg';
+import Leadership02 from 'assets/images/team/leadership/leadership-02.jpg';
+import Leadership03 from 'assets/images/team/leadership/leadership-03.jpg';
+import Leadership04 from 'assets/images/team/leadership/leadership-04.jpeg';
+import Leadership05 from 'assets/images/team/leadership/leadership-05.jpg';
+
+import Advisor01 from 'assets/images/team/advisor/advisor-01.jpg';
+import Advisor02 from 'assets/images/team/advisor/advisor-02.jpg';
+import Advisor03 from 'assets/images/team/advisor/advisor-03.jpg';
+import Advisor04 from 'assets/images/team/advisor/advisor-04.jpg';
+import Advisor05 from 'assets/images/team/advisor/advisor-05.jpg';
+
+import Team01 from 'assets/images/team/team/team-01.jpg';
+import Team02 from 'assets/images/team/team/team-02.jpg';
+import Team03 from 'assets/images/team/team/team-03.jpg';
+import Team04 from 'assets/images/team/team/team-04.jpg';
+import Team05 from 'assets/images/team/team/team-05.jpg';
+import Team06 from 'assets/images/team/team/team-06.jpg';
+import Team07 from 'assets/images/team/team/team-07.jpg';
+import Team08 from 'assets/images/team/team/team-08.jpg';
+import Team09 from 'assets/images/team/team/team-09.jpg';
+
+const TeamsMember = {
+  leadership: [
+    {
+      name: 'Kiyoshi Kuro',
+      image: Leadership01,
+      position: 'CEO and Founder',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: 'https://www.linkedin.com/in/kiyoshi-kuro-3596a5243/' }],
+    },
+    {
+      name: 'Hiroto Kiyama',
+      image: Leadership02,
+      position: 'CTO and Co-Founder',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: 'https://www.linkedin.com/in/datingfi-go-965599243/' }],
+    },
+    {
+      name: 'Hiren Fald',
+      image: Leadership03,
+      position: 'CRO',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: 'https://www.linkedin.com/in/hiren-faldu-117745243/' }],
+    },
+    {
+      name: 'Jae-Eun',
+      image: Leadership04,
+      position: 'Content strategy',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+    {
+      name: 'Satoshi Okabe',
+      image: Leadership05,
+      position: 'CSO',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+  ],
+  advisor: [
+    {
+      name: 'Hiroyuki Nakagawa',
+      image: Advisor01,
+      position: 'Strategic Advisor',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+    {
+      name: 'Toshio Suzuki',
+      image: Advisor02,
+      position: 'Executive Advisor',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+    {
+      name: 'Yasuhiko Fujisawa',
+      image: Advisor03,
+      position: 'Finalcial Advisor',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+    {
+      name: 'Kaori Masui',
+      image: Advisor04,
+      position: 'Advisor',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+    {
+      name: 'Ryoko Maeda',
+      image: Advisor05,
+      position: 'Advisor',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+  ],
+  teams: [
+    {
+      name: 'Healer T',
+      image: Team01,
+      position: 'Solution Architechture',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: 'https://www.linkedin.com/in/healer-t-069073205/' }],
+    },
+    {
+      name: 'Linh Aji',
+      image: Team02,
+      position: 'Mobile Developer Expert',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+    {
+      name: 'EbiSan',
+      image: Team03,
+      position: 'Full Stack Developer',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+    {
+      name: 'Jin Sogami',
+      image: Team04,
+      position: 'Senior Backend',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+    {
+      name: 'Hiroyuki Ikeuchi',
+      image: Team05,
+      position: 'Senior Backend',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+    {
+      name: 'Leo',
+      image: Team06,
+      position: 'Senior FontEnd',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+    {
+      name: 'Ayumi Takei',
+      image: Team07,
+      position: 'UI/UX Design',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+    {
+      name: 'Masaki Shioda',
+      image: Team08,
+      position: 'Senior Blockchain',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+    {
+      name: 'Masakazu Karasaki',
+      image: Team09,
+      position: 'Senior Fontend',
+      socials: [{ facebook: '' }, { twitter: '' }, { linkedin: '' }],
+    },
+  ],
+};
+
+export default TeamsMember;
