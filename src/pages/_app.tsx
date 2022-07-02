e9750257484c9571707ce2c719ae2e3af4e22ba4
camnguyen37h <camnguyen37h@gmail.com>
2022-07-03 00:56:54 +0700

    developmennt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { ReactElement, ReactNode } from 'react';
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import type { NextPage } from 'next';
+import AOS from 'aos';
 import 'assets/styles/godatingfi.scss';
 import 'aos/dist/aos.css';
 import { reload } from 'utils/functions';
@@ -18,6 +20,14 @@ type AppPropsWithLayout = AppProps & {
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>);
+
+  useEffect(() => {
+    AOS.init({
+      easing: 'ease-out-cubic',
+      once: true,
+      offset: 50,
+    });
+  }, []);
 
   return (
     <ErrorBoundary onReset={reload}>
